89637c1fd13d778812813d94aba69c8ba2b358e2
nick_richardson <nick.richardson@rezonence.com>
2021-05-07 15:14:37 +0100

    Give button and widgets rounded corners

--- src/configurator/Configurator.svelte
+++ src/configurator/Configurator.svelte
@@ -17,23 +17,28 @@
     generatePoem();
 </script>
 
-<div class="options-container">
+<div id="options-container">
     <label for="rhyme-scheme">Rhyme Scheme:</label>
     <input type="text" id="rhyme-scheme" bind:value={rhymeScheme}>
     <label for="line-length">Line length:</label>
     <input type="number" id="line-length" min="0" max={maxLineLength} bind:value={lineLength}>
-    <button on:click={generatePoem}>Generate</button>
+    <button on:click={generatePoem} id="generate-button">Generate</button>
 </div>
 
 <style>
-    .options-container {
+    #options-container {
         display: flex;
         flex-direction: row;
         justify-content: center;
         align-items: baseline;
     }
 
-    .options-container > * {
+    #options-container > * {
         margin: 3px;
+        border-radius: 10px;
+    }
+
+    #generate-button {
+        color: #006060;
     }
 </style>
